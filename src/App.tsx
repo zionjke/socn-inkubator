ebec0db588854cad1f19a1e16f1a1d414c93eaee
zionjke <vincere802@gmail.com>
2021-02-08 20:51:48 +0200

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
